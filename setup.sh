afd806645be94b53ac7b7b9963ce84578944af5f
Michael Deisher <michael.deisher@azimuth-ai.com>
2026-03-10 11:08:22 -0700

    added GRU SOC example

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ export FREERTOS_PATH=$PWD/deps/FreeRTOS-Kernel
 export PICO_SDK_PATH=$PWD/deps/pico-sdk
 
 echo "add_subdirectory(\"examples/freertos_nn\")" >> deps/pico-tflmicro/CMakeLists.txt
+echo "add_subdirectory(\"examples/freertos_gru\")" >> deps/pico-tflmicro/CMakeLists.txt
 ln -s $PWD/examples/freertos_nn deps/pico-tflmicro/examples/freertos_nn
+ln -s $PWD/examples/freertos_gru deps/pico-tflmicro/examples/freertos_gru
 
