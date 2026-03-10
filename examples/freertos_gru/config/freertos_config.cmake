a0ade0a314d07dd23ab3716756fd85af578d35be
Michael Deisher <michael.deisher@azimuth-ai.com>
2026-03-10 11:12:44 -0700

    added GRU SOC example

--- examples/freertos_gru/config/freertos_config.cmake
+++ examples/freertos_gru/config/freertos_config.cmake
@@ -1,17 +1,17 @@
 cmake_minimum_required(VERSION 3.15)
 
-add_library(freertos_config STATIC)
+add_library(freertos_gru_config STATIC)
 
-target_sources(freertos_config PUBLIC   
+target_sources(freertos_gru_config PUBLIC   
         ${CMAKE_CURRENT_LIST_DIR}/IdleMemory.c
     )
-target_include_directories(freertos_config PUBLIC
+target_include_directories(freertos_gru_config PUBLIC
 	${CMAKE_CURRENT_LIST_DIR}
 	) # The config file directory\n"
-target_compile_definitions(freertos_config
+target_compile_definitions(freertos_gru_config
 	PUBLIC  projCOVERAGE_TEST=0
 	)
-target_link_libraries(freertos_config
+target_link_libraries(freertos_gru_config
 	FreeRTOS-Kernel
 	pico_stdlib
 	)
