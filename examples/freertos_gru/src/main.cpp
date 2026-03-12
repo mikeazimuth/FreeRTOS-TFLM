c0c839b763153f147efca8fdd7ad3bed7241b846
Mike Deisher <michael.deisher@azimuth-ai.com>
2026-03-12 14:42:14 -0700

    code refactoring

--- examples/freertos_gru/src/main.cpp
+++ examples/freertos_gru/src/main.cpp
@@ -5,47 +5,10 @@
 
 #include <cstdio>
 #include "BlinkAgent.h"
-#include "tensorflow/lite/micro/micro_interpreter.h"
-#include "tensorflow/lite/micro/micro_log.h"
-#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
-#include "tensorflow/lite/micro/system_setup.h"
-#include "tensorflow/lite/schema/schema_generated.h"
-#ifdef INT8_MODEL
-#include "model_int8.h"
-#define ARENA_SIZE 16384
-#else
-#include "model_fp32.h"
-#define ARENA_SIZE 32768
-#endif
-#define NUM_INPUTS 4
-
-// Globals, used for compatibility with Arduino-style sketches.
-namespace {
-  const tflite::Model* model = nullptr;
-  tflite::MicroInterpreter* interpreter = nullptr;
-  TfLiteTensor* input = nullptr;
-  TfLiteTensor* output = nullptr;
-  unsigned int cycle_count = 0;
-  unsigned int inference_count = 0;
-  const int kInferencesPerCycle = 100;
-  constexpr int kTensorArenaSize = ARENA_SIZE;
-  int num_inputs = NUM_INPUTS;
-  uint8_t tensor_arena[kTensorArenaSize];
-}  // namespace
-
+#include "NnetAgent.h"
 
 #define TASK_PRIORITY ( tskIDLE_PRIORITY + 1UL ) //Standard Task priority
 #define GP_BLINK 25
-
-#define INSERT_RESOLVER(a) \
-  { \
-    TfLiteStatus resolve_status = a; \
-    if (resolve_status != kTfLiteOk) { \
-      printf("Op resolution failed"); \
-      return; \
-    } \
-  }
-
 
 /***
  * Debug function to look at Task Stats
@@ -94,128 +57,47 @@ void runTimeStats() {
 	 );
 }
 
-
 /***
  * Main task to boot the other Agents
  * @param params - unused
  */
 void mainTask(void *params){
   BlinkAgent blink(GP_BLINK);
+  NnetAgent nnet(NNET_DELAY);
 
   printf("Boot task started\n");
+
   blink.start("Blink", TASK_PRIORITY);
+  //nnet.start("Nnet", TASK_PRIORITY);
 
   for (;;){
-    
-#ifdef INT8_MODEL
-    for(int i = 0; i < num_inputs; i++) {
-      input->data.int8[i] = (char)i;
-    }
-#else
-    for(int i = 0; i < num_inputs; i++) {
-      input->data.f[i] = (float)i;
-    }
-#endif
-    
-    // Run inference, and report any error
-    TfLiteStatus invoke_status = interpreter->Invoke();
-    if (invoke_status != kTfLiteOk) {
-      printf("Invoke %u failed\n", inference_count);
-      return;
-    }
-
-    // Read output SOC estimate and print to console
-    float soc = 0.0;
-#ifdef INT8_MODEL
-    soc = (float) output->data.int8[0];
-#else
-    soc = output->data.f[0];
-#endif
-
-    // Increment the inference_counter, dump results once per cycle
-    if ((inference_count % kInferencesPerCycle) == 0) {
-      printf("soc=%f, count=%d\n", soc, inference_count);
-      cycle_count += 1;
-    }
-    inference_count += 1;
-    
     vTaskDelay(2000);
     runTimeStats();
   }
 }
-
 
 /***
  * Launch the tasks and scheduler
  */
 void vLaunch( void) {
 
-	//Start blink task
-    TaskHandle_t task;
-    xTaskCreate(mainTask, "MainThread", 500, NULL, TASK_PRIORITY, &task);
+  printf("vLaunch: creating main task\n");
+  
+  //Start main task and sub-tasks
+  TaskHandle_t task;
+  xTaskCreate(mainTask, "MainThread", 500, NULL, TASK_PRIORITY, &task);
+  
+  printf("vLaunch: starting scheduler\n");
 
-    /* Start the tasks and timer running. */
-    vTaskStartScheduler();
+  // Start the tasks and timer running
+  vTaskStartScheduler();
+
+  printf("vLaunch: done\n");
 }
-
-
-// The name of this function is important for Arduino compatibility.
-void setup() {
-  tflite::InitializeTarget();
-
-  // Map the model into a usable data structure. This doesn't involve any
-  // copying or parsing, it's a very lightweight operation.
-  model = tflite::GetModel(g_float_model_data);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
-    printf("Model provided is schema version %d not equal "
-	   "to supported version %d.",
-	   model->version(), TFLITE_SCHEMA_VERSION);
-    return;
-  }
-
-  // This pulls in all the operation implementations we need.
-  static tflite::MicroMutableOpResolver<10> resolver;
-  INSERT_RESOLVER(resolver.AddSlice())
-  INSERT_RESOLVER(resolver.AddFullyConnected())
-  INSERT_RESOLVER(resolver.AddAdd())
-  INSERT_RESOLVER(resolver.AddLogistic())
-  INSERT_RESOLVER(resolver.AddMul())
-  INSERT_RESOLVER(resolver.AddTanh())
-  INSERT_RESOLVER(resolver.AddSub())
-  INSERT_RESOLVER(resolver.AddRelu())
-  INSERT_RESOLVER(resolver.AddConcatenation())
-  INSERT_RESOLVER(resolver.AddReshape())
-
-  // Build an interpreter to run the model with.
-  static tflite::MicroInterpreter static_interpreter(
-      model, resolver, tensor_arena, kTensorArenaSize);
-  interpreter = &static_interpreter;
-
-  // Allocate memory from the tensor_arena for the model's tensors.
-  TfLiteStatus allocate_status = interpreter->AllocateTensors();
-  if (allocate_status != kTfLiteOk) {
-    printf("AllocateTensors() failed");
-    return;
-  }
-
-  // Obtain pointers to the model's input and output tensors.
-  input = interpreter->input(0);
-  output = interpreter->output(0);
-
-  // Keep track of how many inferences we have performed.
-  cycle_count = 0;
-  inference_count = 0;
-
-  printf("Setup complete\n");
-}
-
-
 
 int main(){
 
   stdio_init_all();
-  sleep_ms(3000);
-  setup();
   sleep_ms(3000);
 
   printf("Go\n");
